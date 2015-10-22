1f5350b1402db01c214fc86bec44c65010ef17da
mincrmatt12 <matthewmirvish@hotmail.com>
2015-10-21 21:08:35 -0400

    Dim?

--- src/main/java/dmf444/RunicBridges/Core/Generation/RuneMine/BiomeGenMine.java
+++ src/main/java/dmf444/RunicBridges/Core/Generation/RuneMine/BiomeGenMine.java
@@ -2,6 +2,8 @@ package dmf444.RunicBridges.Core.Generation.RuneMine;
 
 import net.minecraft.world.biome.BiomeGenBase;
 
+import java.util.ArrayList;
+
 /**
  * Created by dmf444 on 10/18/2015.
  */
@@ -11,5 +13,7 @@ public class BiomeGenMine extends BiomeGenBase {
     public BiomeGenMine(int par1) {
         super(par1);
         this.setBiomeName("RuneEssenceMine");
+        this.spawnableMonsterList = new ArrayList<>();
+
     }
 }
