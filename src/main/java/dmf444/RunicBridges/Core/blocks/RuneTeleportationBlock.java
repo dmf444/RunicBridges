fe2bd84e7521bd29e16f6532db0a76b8636f9c1a
dmf444 <becat444@gmail.com>
2015-10-22 19:41:29 -0400

    Commit of teleportation

--- src/main/java/dmf444/RunicBridges/Core/blocks/RuneTeleportationBlock.java
+++ src/main/java/dmf444/RunicBridges/Core/blocks/RuneTeleportationBlock.java
@@ -1,17 +1,49 @@
 package dmf444.RunicBridges.Core.blocks;
 
 import dmf444.RunicBridges.Core.Lib.BlockLib;
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.Teleporter;
+import net.minecraft.world.World;
 
 /**
  * Created by David on 2015-10-22.
  */
-public class RuneTeleportationBlock extends Block {
+public class RuneTeleportationBlock extends BlockContainer {
 
     public RuneTeleportationBlock(){
         super(Material.snow);
         this.setBlockUnbreakable();
         this.setBlockTextureName(BlockLib.bTeleport);
+    }
+
+    public void onEntityWalking(World world, int x, int y, int z, Entity entity) {
+        if(entity instanceof EntityPlayer){
+            if(entity.ridingEntity == null && entity.riddenByEntity == null && entity instanceof EntityPlayerMP){
+                EntityPlayerMP thePlayer = (EntityPlayerMP) entity;
+
+                MinecraftServer server = MinecraftServer.getServer();
+                if(entity instanceof EntityPlayerMP){
+                    if(thePlayer.timeUntilPortal > 0){
+                        thePlayer.timeUntilPortal = 10;
+                    }else if(thePlayer.dimension == -4412){
+                        thePlayer.timeUntilPortal = 10;
+                        thePlayer.mcServer.getConfigurationManager().transferPlayerToDimension(thePlayer, 0, new Teleporter(server.worldServerForDimension(0)));
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World world, int side) {
+        return null;
     }
 }
---
+++ src/main/java/dmf444/RunicBridges/Core/blocks/tileentity/TileRuneTeleportation.java
@@ -0,0 +1,13 @@
+package dmf444.RunicBridges.Core.blocks.tileentity;
+
+import net.minecraft.tileentity.TileEntity;
+
+/**
+ * Created by David on 2015-10-22.
+ */
+public class TileRuneTeleportation extends TileEntity{
+
+    public TileRuneTeleportation(){
+        //DO NOTHING FOR THINGS
+    }
+}
